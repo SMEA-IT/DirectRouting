f379e8da4c1aba5d67438645e27bc581bc1bcdcd
SMEA-IT <69198716+SMEA-IT@users.noreply.github.com>
2020-10-06 10:59:44 +0200

    Update DirektRouting_CarrierTrunk.ps1

--- DirektRouting_CarrierTrunk.ps1
+++ DirektRouting_CarrierTrunk.ps1
@@ -7,8 +7,8 @@ $voicerouteidentity2 = "SBC2 - HFO"
 $numberpatternInternational = "^(\+\d*)"
 $voiceroutingpolicyname = "World"
 $pfadzurCSV = "C:\pfadzurcsv\TeamsUser.csv"
-$sbc1 = "Kunde.sbc1.getdirectrouting.de"
-$sbc2 = "Kunde.sbc2.getdirectrouting.de"
+$sbc1 = "kunden.sbc1.getdirectrouting.de"
+$sbc2 = "kunden.sbc2.getdirectrouting.de"
 
 # Import SkypeOnlineConnector Modul / Check ob SkypeOnlineConnector Modul installiert ist 
 if (Get-Module -ListAvailable -Name SkypeOnlineConnector)
@@ -21,28 +21,48 @@ else
 
 }
 # Sitzung erstellen und verbinden
-$session = New-CsOnlineSession
+$session = New-CsOnlineSession  
 Import-PSSession $session -AllowClobber
 
 # CSV Datei einlesen
 $teamscsvcontent = Import-Csv -path $pfadzurCSV -Delimiter ";"
-
-# Benutzer aktivieren und verwalten
-foreach ($teamsuser in $teamscsvcontent) {
+try
+{
+    # Benutzer aktivieren und verwalten
+    foreach ($teamsuser in $teamscsvcontent) {
     Set-CsUser -Identity $teamsuser.mail -EnterpriseVoiceEnabled $true -HostedVoiceMail $true -OnPremLineURI $teamsuser.phone
     Write-Host -ForegroundColor Green $teamsuser.mail " Enterprise Voice Enabled und Telefonnummer zugewiesen"
+    }
+}
+catch
+{
+    Write-Host -ForegroundColor Red $_.Exception
 }
 
 
 # Konfiguration Voice Routing
-Set-CsOnlinePstnUsage  -Identity Global -Usage @{Add=$pstnusageInternational}
-New-CsOnlineVoiceRoute -Identity $voicerouteidentity1 -NumberPattern $numberpatternInternational -OnlinePstnGatewayList $sbc1 -Priority 1 -OnlinePstnUsages $pstnusageInternational
-New-CsOnlineVoiceRoute -Identity $voicerouteidentity2 -NumberPattern $numberpatternInternational -OnlinePstnGatewayList $sbc2 -Priority 2 -OnlinePstnUsages $pstnusageInternational
-New-CsOnlineVoiceRoutingPolicy $voiceroutingpolicyname -OnlinePstnUsages $pstnusageInternational
+try 
+{
+    Set-CsOnlinePstnUsage  -Identity Global -Usage @{Add=$pstnusageInternational}
+    New-CsOnlineVoiceRoute -Identity $voicerouteidentity1 -NumberPattern $numberpatternInternational -OnlinePstnGatewayList $sbc1 -Priority 1 -OnlinePstnUsages $pstnusageInternational
+    New-CsOnlineVoiceRoute -Identity $voicerouteidentity2 -NumberPattern $numberpatternInternational -OnlinePstnGatewayList $sbc2 -Priority 2 -OnlinePstnUsages $pstnusageInternational
+    New-CsOnlineVoiceRoutingPolicy $voiceroutingpolicyname -OnlinePstnUsages $pstnusageInternational
+}
+catch 
+{
+    Write-Host -ForegroundColor Red $_.Exception
+}
 
-foreach ($user in $teamscsvcontent) {
-    Grant-CsOnlineVoiceRoutingPolicy -Identity $user.mail -PolicyName $voiceroutingpolicyname
-    Grant-CsDialOutPolicy -Identity $user.mail -PolicyName "Tag:DialoutCPCandPSTNInternational"
-    Write-Host -ForegroundColor Green $user.mail " RoutingPolicy zugewiesen"
+try 
+{
+    foreach ($user in $teamscsvcontent) {
+        Grant-CsOnlineVoiceRoutingPolicy -Identity $user.mail -PolicyName $voiceroutingpolicyname
+        Grant-CsDialOutPolicy -Identity $user.mail -PolicyName "Tag:DialoutCPCandPSTNInternational"
+        Write-Host -ForegroundColor Green $user.mail " RoutingPolicy zugewiesen"
+    }
+}
+catch 
+{
+    Write-Host -ForegroundColor Red $_.Exception
 }
 Remove-PSSession $session
