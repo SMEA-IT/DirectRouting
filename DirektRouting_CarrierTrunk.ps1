8638e727d522bfa530348c03533d03921d543118
SMEA-IT <69198716+SMEA-IT@users.noreply.github.com>
2021-05-17 15:34:46 +0200

    Update DirektRouting_CarrierTrunk.ps1

--- DirektRouting_CarrierTrunk.ps1
+++ DirektRouting_CarrierTrunk.ps1
@@ -17,7 +17,7 @@ if (Get-Module -ListAvailable -Name MicrosoftTeams)
 }
 else
 { 
-    Install-Module -Name MicrosoftTeams -AllowClobber
+    Install-Module -Name MicrosoftTeams -AllowClobber -RequiredVersion 2.0.0
     Import-Module MicrosoftTeams
 
 }
