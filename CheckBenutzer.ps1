d6f85af120db24bbdc4aa7ddbeaf87f12ecdfa82
SMEA-IT <69198716+SMEA-IT@users.noreply.github.com>
2022-05-02 14:01:35 +0200

    update checkbenutzer
    new Teams Module version

--- CheckBenutzer.ps1
+++ CheckBenutzer.ps1
@@ -5,7 +5,7 @@ if (Get-Module -ListAvailable -Name MicrosoftTeams)
 }
 else
 { 
-    Install-Module -Name MicrosoftTeams -AllowClobber -RequiredVersion 3.0.0
+    Install-Module -Name MicrosoftTeams -AllowClobber -RequiredVersion 4.2.0
     Import-Module MicrosoftTeams
 
 }
