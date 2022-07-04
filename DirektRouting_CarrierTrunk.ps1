403e9be6a129113f627cf439909a69132b93e7b1
SMEA-IT <69198716+SMEA-IT@users.noreply.github.com>
2022-07-04 09:20:43 +0200

    carrier trunk
    module version

--- DirektRouting_CarrierTrunk.ps1
+++ DirektRouting_CarrierTrunk.ps1
@@ -17,7 +17,7 @@ if (Get-Module -ListAvailable -Name MicrosoftTeams)
 }
 else
 { 
-    Install-Module -Name MicrosoftTeams -AllowClobber -RequiredVersion 4.2.0
+    Install-Module -Name MicrosoftTeams -AllowClobber -RequiredVersion 4.4.0
     Import-Module MicrosoftTeams
 
 }
