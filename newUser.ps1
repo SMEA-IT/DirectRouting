be33297db927ba9330d0a0c3eec20e684feadc69
SMEA-KM <111292143+SMEA-KM@users.noreply.github.com>
2023-05-22 15:59:02 +0200

    Update newUser.ps1
    Mod Line 12, New Teams Version

--- newUser.ps1
+++ newUser.ps1
@@ -9,7 +9,7 @@ if (Get-Module -ListAvailable -Name MicrosoftTeams)
 }
 else
 { 
-    Install-Module -Name MicrosoftTeams -AllowClobber -RequiredVersion 4.4.0
+    Install-Module -Name MicrosoftTeams -AllowClobber -RequiredVersion 4.7.0
     Import-Module MicrosoftTeams
 
 }
